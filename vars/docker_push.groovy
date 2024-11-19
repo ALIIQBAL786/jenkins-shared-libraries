ab14e7c85d6d708423d4287841ad837ce34be9f6
Ali Iqbal <97777690+ALIIQBAL786@users.noreply.github.com>
2024-11-19 10:59:06 +0500

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -2,5 +2,5 @@ def call(String Project, String ImageTag,String DockerHub){
 withCredientials(usernamePassword(credentialsId:"DockerHub","dockerHubpass",usernameVariable:"DockerHubUser"){
 sh "docker login -u ${DockerHubUser} -p ${dockerHubpass}"
 }
-sh"docker push ${DockerHubUser}/${Project}:${ImageTag}"
+sh "docker push ${DockerHubUser}/${Project}:${ImageTag}"
 }
